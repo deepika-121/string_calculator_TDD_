61271cc492338f89af7621b515f644a24f18fd46
= <=>
2024-05-23 12:00:51 +0530

    handle negative numbers

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -29,4 +29,12 @@ RSpec.describe StringCalculator do
   it "supports different delimiters" do
     expect(@calculator.add("//;\n1;2")).to eq(3)
   end
+
+  it "raises an exception for negative numbers" do
+    expect { @calculator.add("1,-2,3") }.to raise_error(RuntimeError)
+  end
+
+  it "raises an exception with a message containing all negative numbers" do
+    expect { @calculator.add("1,-2,-3") }.to raise_error("negative numbers not allowed: -2,-3")
+  end
 end
